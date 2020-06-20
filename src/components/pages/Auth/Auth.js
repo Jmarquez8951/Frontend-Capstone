898747aff49631940ad3986e10f727a70cace41b
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-20 14:17:04 -0500

    got the start of the navbar

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -15,7 +15,7 @@ class Auth extends React.Component {
     return (
       <div>
         <h1>Auth</h1>
-        <button className="btn btn-danger" onClick={this.loginClickEvent}><i class="fab fa-google"></i> Log in</button>
+        <button className="btn btn-danger" onClick={this.loginClickEvent}><i className="fab fa-google"></i> Log in</button>
       </div>
     );
   }
