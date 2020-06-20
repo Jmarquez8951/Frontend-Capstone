457b5d503e206164ba80454d6080ff2f0352b5cc
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-20 12:48:51 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>GameSafe</h1>
+        <button className="btn btn-warning"><i class="fas fa-air-freshener"></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
