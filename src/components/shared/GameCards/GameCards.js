c6554521a2a03a2236a75378f2c8edff532c0880
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-22 21:31:59 -0500

    got the cards to populate onto the page

--- src/components/shared/GameCards/GameCards.js
+++ src/components/shared/GameCards/GameCards.js
@@ -11,11 +11,22 @@ class GameCards extends React.Component {
 
   render() {
     const { game } = this.props;
-    const singleLink = `/game/${game.id}`;
+    const singleLink = `/game/${game.slug}`;
+    const gameGenres = game.genres.map((genre) => (
+      <p key={genre.id} className="text-white bg-info border border-dark rounded m-1 pr-2 pl-2">{genre.name}</p>
+    ));
     return (
       <div className="GameCards m-3 col-3 d-flex">
-        <h4>{game.name}</h4>
-        <Link className="btn btn-dark m-1" to={singleLink}>Single Link</Link>
+        <div className="card bg-dark">
+          <img src={game.short_screenshots[0].image} className="card-img-top border border-dark rounded" alt="game artwork"/>
+          <div className="card-body">
+            <h5 className="card-title text-white">{game.name}</h5>
+            <div className="d-flex flex-wrap justify-content-center">
+              {gameGenres}
+            </div>
+            <Link className="btn btn-secondary m-1" to={singleLink}>More Info</Link>
+          </div>
+        </div>
       </div>
     );
   }
