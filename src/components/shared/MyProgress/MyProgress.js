6d08ec18c58085aca745359c790724e319a25d36
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-27 10:51:53 -0500

    got the progress bar to work properly

--- src/components/shared/MyProgress/MyProgress.js
+++ src/components/shared/MyProgress/MyProgress.js
@@ -11,14 +11,16 @@ class MyProgress extends React.Component {
 
   render() {
     const { ratings } = this.props;
-
     return (
       <div className="MyProgress mt-3 mb-3">
-        <Progress multi>
-          {ratings.title === 'exceptional'
-            ? <Progress bar color="success" value={ratings.percent}>{ratings.title}</Progress>
-            : '' }
-        </Progress>
+          {ratings
+            ? <Progress multi>
+                <Progress bar className={ratings[0].title} value={ratings[0].percent}>{ratings[0].title.charAt(0).toUpperCase() + ratings[0].title.slice(1)}</Progress>
+                <Progress bar className={ratings[1].title} value={ratings[1].percent}>{ratings[1].title.charAt(0).toUpperCase() + ratings[1].title.slice(1)}</Progress>
+                <Progress bar className={ratings[2].title} value={ratings[2].percent}>{ratings[2].title.charAt(0).toUpperCase() + ratings[2].title.slice(1)}</Progress>
+                <Progress bar className={ratings[3].title} value={ratings[3].percent}>{ratings[3].title.charAt(0).toUpperCase() + ratings[3].title.slice(1)}</Progress>
+              </Progress>
+            : ''}
       </div>
     );
   }
