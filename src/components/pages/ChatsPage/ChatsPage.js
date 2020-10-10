189e803597960095022538a540c07619bf377f2c
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-10-10 11:46:52 -0500

    got the discussions page to populate correctly

--- src/components/pages/ChatsPage/ChatsPage.js
+++ src/components/pages/ChatsPage/ChatsPage.js
@@ -3,10 +3,12 @@ import './ChatsPage.scss';
 
 import chatsData from '../../../helpers/data/chatsData';
 import DiscussionCard from '../../shared/DiscussionCard/DiscussionCard';
+import usersData from '../../../helpers/data/usersData';
 
 class ChatsPage extends React.Component {
   state = {
     discussions: [],
+    users: [],
   }
 
   getDiscussions = () => {
@@ -17,15 +19,29 @@ class ChatsPage extends React.Component {
       .catch((err) => console.error('Could not get discussions', err));
   }
 
+  getUsers = () => {
+    usersData.getAllUsers()
+      .then((response) => {
+        this.setState({ users: response });
+      })
+      .catch((err) => console.error('could not get users', err));
+  }
+
   componentDidMount() {
     this.getDiscussions();
+    this.getUsers();
   }
 
   render() {
-    const { discussions } = this.state;
+    const { discussions, users } = this.state;
 
     const buildDiscussionCards = discussions.map((discussion) => (
-      <DiscussionCard discussion={discussion} />
+      users.map((user) => {
+        if (user.uid === discussion.uid) {
+          return <DiscussionCard discussion={discussion} user={user}/>;
+        }
+        return '';
+      })
     ));
 
     return (
--- src/components/shared/DiscussionCard/DiscussionCard.js
+++ src/components/shared/DiscussionCard/DiscussionCard.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './DiscussionCard.scss';
 
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class DiscussionCard extends React.Component {
   static propTypes = {
     discussion: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
   }
 
   render() {
-    const { discussion } = this.props;
+    const { discussion, user } = this.props;
 
     return (
-      <div className="DiscussionCard">
-        <h1>{discussion.topic}</h1>
+      <div className="DiscussionCard m-3">
+        <div className="card">
+          <h5 className="card-header">{discussion.topic}</h5>
+          <div className="card-body flex justify-content-between">
+            <Link to={`/chat/${discussion.id}`} className="btn btn-primary align-self-end">Go to Discussion</Link>
+          </div>
+          <footer className="flex flex-wrap">
+            <small className="card-text ">Created by {user.username}</small>
+          </footer>
+        </div>
       </div>
     );
   }
